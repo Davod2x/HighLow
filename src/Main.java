5ffc8ae8183e66fb1465a89999805c4060323992
David <ddlukula21@coe.edu>
2023-01-12 13:12:15 -0600

    2nd attempt but a bit stuck

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,21 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Pick a number from 1- 50");
         String userGuess = myObj.nextLine();
-        int i =Integer.parseInt(userGuess);
+        int i = Integer.parseInt(userGuess);
 
         int a = randnum;
 
-        while (i != randnum)
-        {
-            if (i > randnum)
-            {
+        while (i != a) {
+            if (i > a) {
                 System.out.println("That number is too big guess again");
                 myObj.nextLine();
+            } if(i < a) {
+                System.out.println("That number is too small guess again");
+                myObj.nextLine();
             }
-            else
-                System.out.println("That number is too big guess again");
-
-
+            }
+        }
 
     }
-}
+
+    }
