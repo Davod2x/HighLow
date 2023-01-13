2c8b5c9d4bfd0687d1cf910be6cca58ce69535d6
David <ddlukula21@coe.edu>
2023-01-13 13:30:46 -0600

    2nd attempt

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,29 @@ public class Main {
         Random rand = new Random();
         int limit = 51;
         int randnum = rand.nextInt(limit);
+        int userGuess = -randnum;
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Pick a number from 1- 50");
-        String userGuess = myObj.nextLine();
-        int i = Integer.parseInt(userGuess);
+        //System.out.println("Pick a number from 1- 50");
+        //userGuess = myObj.nextLine();
+        //int i = Integer.parseInt(userGuess);
 
-        int a = randnum;
 
-        while (i != a) {
-            if (i > a) {
+        while (userGuess != randnum ) {
+            System.out.println("Pick a number from 1- 50");
+            userGuess = myObj.nextInt();
+            if (userGuess > randnum) {
                 System.out.println("That number is too big guess again");
-                myObj.nextLine();
-            } if(i < a) {
+             //   userGuess = myObj.nextLine();
+            } if(userGuess < randnum) {
                 System.out.println("That number is too small guess again");
-                myObj.nextLine();
+              //  userGuess = myObj.nextLine();
+            }
+            else{
+                System.out.println("Correct");
             }
             }
         }
 
     }
 
-    }
+
